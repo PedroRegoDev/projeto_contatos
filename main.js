f2e0bf7d8a0a4e250d453ab9363165ee211b3ae9
Pedro Henrique <pro.motossta@gmail.com>
2024-02-15 09:11:04 -0300

    commit principal

---
+++ main.js
@@ -0,0 +1,42 @@
+const form = document.getElementById('form-contato');
+const nomes = [];
+const numeros = [];
+
+let linhas = [];
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha() {
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputNumeroContato = document.getElementById('numero-contato');
+
+    if (nomes.includes(inputNomeContato.value)) {
+        alert(`Este contato: ${inputNomeContato.value} já foi inserido na sua agenda`);
+    }
+
+    else {
+        nomes.push(inputNomeContato.value);
+        numeros.push(inputNumeroContato.value);
+
+        let linha = '<tr>';
+        linha += `<td>${inputNomeContato.value}</td>`;
+        linha += `<td>${inputNumeroContato.value}</td>`;
+        linha += `<td>Cadastrado</td>`;
+        linha += '</tr>';
+
+        linhas += linha;
+    }
+
+    inputNomeContato.value = '';
+    inputNumeroContato.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
